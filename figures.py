ff41f47cc3079bc1b3b0e9b03ea0e2ca19873521
katiemedina <59849581+katiemedina@users.noreply.github.com>
2020-04-26 23:37:25 -0400

    Update figures.py

--- figures.py
+++ figures.py
@@ -19,43 +19,24 @@ buses = buses.sort_values(['pct_reliable'], ascending = True)
 buses = buses.reset_index()
 
 #%%
-
-plt.figure()
-sns.set(style = 'whitegrid')
-fig1 = sns.barplot(data = buses, x = 'gtfs_route_id', y = 'pct_reliable')
-
-
-#%%
-#PLOT BEST AND WORST RELIABLE
-#NEED TO SORT PROPERLY SO DISPLAY LOWEST TO HIGHEST
+#Reliability and Ridership
 plt.subplots_adjust(hspace = 0.45)
 sns.set(style='whitegrid')
+
+#Reliability
 plt.subplot(2,1,1)
-fg1 = sns.barplot(x = 'gtfs_route_id', y = 'pct_reliable', data=buses, color = 'salmon')
-fg1.set_ylim([0,100])
-fg1.set_title("Reliability")
-fg1.set(xlabel = 'Route ID', ylabel = 'Percentage On Time')
+sns.set(style = 'whitegrid')
+fg1 = sns.barplot(x='gtfs_route_id', y="pct_reliable", data=buses, 
+            order=buses.sort_values('pct_reliable').gtfs_route_id, color = 'salmon')
+fg1.set(xlabel = 'Route ID', ylabel = 'Percent Reliable')
+fg1.set_title('Reliability by Route')
 
+#Ridership
 plt.subplot(2,1,2)
-fg2 = sns.barplot(x = 'gtfs_route_id', y = 'boardings', data=buses, color = 'green')
-fg2.set_title("Ridership")
+fg2 = sns.barplot(x='gtfs_route_id', y="boardings", data=buses, order=buses.sort_values('boardings').gtfs_route_id, color = 'blue')
 fg2.set(xlabel = 'Route ID', ylabel = 'Typical Boardings')
-
-
-
-#ax = sns.barplot(x = 'gtfs_route_id', y = 'pct_reliable', data = reliability2018, hue = 'peak_offpeak_ind')
-
-#%%
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax2 = ax.twinx()
-
-width = 0.4
-buses.pct_reliable.plot(kind = 'bar', color = 'red', ax = ax, width = width, position = 1)
-buses.boardings.plot(kind = 'bar', color = 'blue', ax = ax2, width = width, position = 0)
-
-plt.show()
+fg2.set_title('Boardings by Route')
+plt.savefig('Reliability and Boardings by Route.png')
 
 #%%
 buses.plot(x = 'gtfs_route_id', kind= 'bar' , secondary_y= 'boardings' , rot= 0 )
