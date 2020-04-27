e9438accbe03ee4e5bc7980d76d589a32f86b0d9
katiemedina <59849581+katiemedina@users.noreply.github.com>
2020-04-26 20:18:36 -0400

    Update bus_ridership.py

--- bus_ridership.py
+++ bus_ridership.py
@@ -13,6 +13,7 @@ zf = zipfile.ZipFile('MBTA_Bus_Ridership_by_Trip_Season_RouteLine_and_Stop.zip')
 #add low_memory = False to deal with mixed type column error
 rides = pd.read_csv(zf.open('MBTA_Bus_Ridership_by_Trip_Season_RouteLine_and_Stop.csv'),low_memory=False)
 
+bad_buses = pd.read_csv('bad_buses.csv')
 #%%
 #subset data to just Fall 2018 rides
 is_2018 = rides['season'] == 'Fall 2018'
@@ -21,22 +22,38 @@ rides18 = rides[is_2018]
 
 #%%
 #BY DIRECTION
-group_line_rides18 = rides18.groupby(['route_id','direction_id'])
+#group_line_rides18 = rides18.groupby(['route_id','direction_id'])
 
-group_line_rides18_sum = group_line_rides18['boardings'].sum()
+#group_line_rides18_sum = group_line_rides18['boardings'].sum()
 
-test = group_line_rides18_sum.unstack()
+#test = group_line_rides18_sum.unstack()
 
-test = test.reset_index()
+#test = test.reset_index()
 
-test['approx_annual_0'] = test[0]*52
+#test['approx_annual_0'] = test[0]*52
 
-test['approx_annual_0'] = test[1]*52
+#test['approx_annual_0'] = test[1]*52
 
 #%%
-#WITHOUT DIRECTION
+#GROUP BY LINE WITHOUT DIRECTION
 glrides18 = rides18.groupby(['route_id'])
 
 glrides18_sum = glrides18['boardings'].sum()
 
+#%%
+#add ridership numbers to bad buses identified in reliability data
+bad_buses['str_route'] = bad_buses['gtfs_route_id'].astype(str)
 
+bus_merged = bad_buses.merge(glrides18_sum,
+                            how = 'left',
+                            left_on = 'str_route',
+                            right_on = 'route_id',
+                            validate = '1:1',
+                            indicator = True)
+
+print('\n')
+print(bus_merged['_merge'].value_counts())
+
+bus_merged.drop(['_merge'], axis = 'columns', inplace = True)
+
+bus_merged.to_csv('rides_and_reliability.csv')
